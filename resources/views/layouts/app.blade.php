4ca44955362762692859ef74db7a12140cfa3c06
Matt Higbee <mhigbee@hotmail.com>
2016-07-20 11:03:40 -0600

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Task List</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head>
+<body>
+	<section class="container">
+		<div>
+			@yield('content')
+
+
+		</div>
+
+	</section>
+
+
+</body>
+</html>
